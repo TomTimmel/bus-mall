82baabd83a0bcf0729e12f4e69b2dda7238e02f3
TomTimmel <tom.t.timmel@gmail.com>
2016-08-30 17:13:14 -0700

    end of day

--- app.js
+++ app.js
@@ -3,34 +3,68 @@
 var imagePaths = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg',
 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 var images = [];
+var currentImageIndices = [0, 1, 2];
 
 for(var i = 0; i < imagePaths.length; i++){
-  var name = imagePaths[i];
-  new Image(null, name);
+  var path = imagePaths[i];
+  var name = imagePaths[i].split('.')[0];
+  new Image(name, path);
 }
-// console.log(imagePaths, images);
+// console.log(images);
 
 var imageList = document.getElementById('images');
 
 imageList.addEventListener('click', clickHandler);
 
-drawImage();
-drawImage();
+drawImage(0);
+drawImage(1);
+drawImage(2);
 
-function clickHandler(e){
-  // console.log(e.target);
+function clickHandler(event){
+  var imageClick = event.target;
+  var imageClick1 = imageClick.getAttribute('src');
+  var arrayOfRandomIndices = randomIndices();
+  // console.log(imageClick, typeof imageClick);
+
+  for (var k = 0; k < currentImageIndices.length; k++){
+    var currentIndex = currentImageIndices[k];
+    var displayedObject = images[currentIndex];
+    displayedObject.views += 1;
+  }
+  // console.log(imageClick1);
+  for (var j = 0; j < images.length; j++){
+    // console.log(images[j].path === imageClick1);
+    if(images[j].path === imageClick1){
+      images[j].clicks += 1;
+    }
+  }
+  currentImageIndices = arrayOfRandomIndices;
   imageList.textContent = '';
-  drawImage();
-  drawImage();
+  drawImage(arrayOfRandomIndices[0]);
+  drawImage(arrayOfRandomIndices[1]);
+  drawImage(arrayOfRandomIndices[2]);
+  // console.log(images);
 }
-function drawImage() {
+
+function randomIndices(){
+  var firstRandomIndex = Math.floor(Math.random() * images.length);
+  var secondRandomIndex = Math.floor(Math.random() * images.length);
+  while (firstRandomIndex === secondRandomIndex){
+    secondRandomIndex = Math.floor(Math.random() * images.length);
+  }
+  var thirdRandomIndex = Math.floor(Math.random() * images.length);
+  while (thirdRandomIndex === firstRandomIndex || thirdRandomIndex === secondRandomIndex){
+    thirdRandomIndex = Math.floor(Math.random() * images.length);
+  }
+  return [firstRandomIndex, secondRandomIndex, thirdRandomIndex];
+}
+function drawImage(index) {
   var img = document.createElement('img');
   var li = document.createElement('li');
   var imageList = document.getElementById('images');
-  var randomIndex = Math.floor(Math.random() * imagePaths.length);
-  var randomPath = imagePaths[randomIndex];
+  var randomPath = images[index].path;
 
-  img.setAttribute('src', 'images/' + randomPath);
+  img.setAttribute('src', randomPath);
 
   li.appendChild(img);
   imageList.appendChild(li);
@@ -40,7 +74,53 @@ function Image(name, path) {
   this.views = 0;
   this.clicks = 0;
   this.name = name;
-  this.path = path;
+  this.path = 'imgs/' + path;
 
   images.push(this);
 }
+
+var label = [];
+for( var m = 0; m < images.length; m++){
+  label.push(images[m].name);
+}
+
+function clickData1(){
+  var clickData = [];
+  for ( var n = 0; n < images.length; n++){
+    clickData.push(images[n].clicks);
+    console.log(clickData1);
+  }return clickData();
+}
+
+var ctx = document.getElementById('my_chart');
+
+var myChart = new Chart(ctx,{
+  type: 'bar',
+  data: {
+    labels: label,
+    datasets: [{
+      label: 'Number of Clicks',
+      data: clickData,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
--- user_stories.md
+++ user_stories.md
@@ -3,12 +3,21 @@
 ** As a developer, I want to create a web page will give the Bus Mall the information on which products to carry in their new catalog. Users will be able to select products they are interested in by clicking on a series of images. The web site will specifically do the following for the Bus Mall company:
 
 - A list of three randomly generated images will show up on the screen at a time.  The user will be directed to select the product that they are most interested in.
-- Each new set of images will be different than the last three, after one interation, the images will be allowed to reused and re-clicked.
-- The user will select 25 images. Once 25 images have been selected, the data of their selections will show up in a chart for analysis.
+- Each new set of images will be different than the last three, after one iteration, the images will be allowed to reused and re-clicked.
+- The user will select 25 images. Once 25 images have been selected, the data of their selections will show up in a chart for analysis.  
 
 ** Development Plan:
 
 -Load the product photos, and get them to randomly display in groups of three.
+
 -Provide the functionality so that images don't repeat in the next set after each click.
 -Provide functionality so that each view and click are captured for analysis.
--Build chart using Chart.js to illustrate the captured views and clicks.
+-Build chart using Chart.js to illustrate the captured views and clicks
+
+** Marketing Research Team
+
+- The marketing research team requires data to help them select the right products to recommend for placement in the new Bus Mall catalog.  They will use the chart created to see which products were clicked on the most times and which products were viewed the most to make their decisions.
+
+** Focus Group Users
+
+- The focus group user will be provided instructions that out-line their role which is to look through 3 pictures of products at a time and select the one they would be most likely to purchase.  They will go through the pictures until they have clicked 25 times. Once they have finished clicking, they can then view the chart to see which products are the most popular to them...
